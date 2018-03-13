ecdc5fea78c9ff15779db956c73f81068b6c182d
Alvin Leung <alvin.leung92@gmail.com>
2018-03-12 21:32:51 -0700

    Add SyntaxValidator class

---
+++ HackVirtualMachine/SyntaxValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HackVirtualMachine
+{
+    static public class SyntaxValidator
+    {
+        static private Regex arithmeticCommandRegex = new Regex(
+            @"^(add|sub|neg|eq|gt|lt|and|or|not)$");
+
+        static private Regex memoryAccessCommandRegex = new Regex(
+            @"(^(push|pop)\s+(local|argument|this|that|static|pointer|temp)\s+\d+$)|(^push\s+constant\s+\d+$)");
+
+        static public bool IsArithmeticVMCommand(string vmCommand)
+        {
+            return arithmeticCommandRegex.IsMatch(vmCommand);
+        }
+
+        static public bool IsMemoryAccessVMCommand(string vmCommand)
+        {
+            return memoryAccessCommandRegex.IsMatch(vmCommand);
+        }
+    }
+}
