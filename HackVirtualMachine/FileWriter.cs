728a2273b63a5c4958879f7619486e2473c59d7a
Alvin Leung <alvin.leung92@gmail.com>
2018-03-20 23:55:51 -0700

    Add FileWriter class

---
+++ HackVirtualMachine/FileWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace HackVirtualMachine
+{
+    public class FileWriter
+    {
+        private string filepath;
+
+        public FileWriter(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        public void Write(IEnumerable<string> assemblyCommands)
+        {
+            FileStream fileStream = new FileStream(this.filepath, FileMode.Create);
+
+            using (StreamWriter writer = new StreamWriter(fileStream))
+            {
+                foreach(string assemblyCommand in assemblyCommands)
+                {
+                    writer.WriteLine(assemblyCommand);
+                }
+            }
+        }
+    }
+}
